c2ff95787ada7c35aa99a12b3dbc3232e69a4fda
nikki <105037571+nikkicabanting@users.noreply.github.com>
2022-05-06 14:27:53 +0800

    i added sayBye method

--- src/App.java
+++ src/App.java
@@ -2,10 +2,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         sayHello();
+        sayBye();
     }
 
     public static void sayHello()
     {
         System.out.println("HELLO EVERYONE!");
+    }
+
+    public static void sayBye() {
+        System.out.println("goodbye!");
     }
 }
