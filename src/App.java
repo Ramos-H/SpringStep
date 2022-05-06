ec32019a7e9cc12cc9b2867c0fdaf7372c3a716e
Hans Ramos <38718084+Hans28git@users.noreply.github.com>
2022-05-06 14:11:41 +0800

    Added "sayHello" method as a test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        sayHello();
+    }
+
+    public static void sayHello()
+    {
+        System.out.println("HELLO EVERYONE!");
     }
 }
