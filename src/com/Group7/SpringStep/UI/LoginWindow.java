d079f0a03753368cab2c69b081ee0e4546631ac1
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2022-05-29 10:34:21 +0800

    Made LoginWindow basic GUI

--- src/com/Group7/SpringStep/UI/LoginWindow.java
+++ src/com/Group7/SpringStep/UI/LoginWindow.java
@@ -5,6 +5,8 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
+import com.Group7.SpringStep.App;
+
 public class LoginWindow extends JFrame
 {
     public LoginWindow()
@@ -23,9 +25,74 @@ public class LoginWindow extends JFrame
         int y = Math.round(screenSize.height / 2 - getHeight() / 2);
         setLocation(x, y);
         {
-            // Put nested components here
+            // Initialize nested components here
+            JPanel mainPanel = new JPanel(new GridBagLayout());
+            mainPanel.setBorder(BorderFactory.createEmptyBorder(0, 150, 0, 150));
+            {
+                JLabel welcomeLabel = new JLabel("Log in to SpringStep!");
+                JTextField userNameField = new JTextField();
+                JTextField passwordField = new JTextField();
+                JCheckBox rememberPasswordCheckBox = new JCheckBox("Remember password");
 
-            //Put the call to add the nested components here
+                JButton signUpButton = new JButton("Sign Up");
+                signUpButton.setBackground(new Color(215, 204, 195));
+
+                JButton logInButton = new JButton("Log In");
+                logInButton.setBackground(new Color(135, 195, 193));
+
+                JLabel iconLogo = new JLabel();
+                iconLogo.setIcon(new ImageIcon(App.resources.get("SpringStep_Logo_Colored_Circle_200x200.png")));
+                
+                JPanel buttonPanel = new JPanel(new FlowLayout());
+                {
+                    buttonPanel.add(signUpButton);
+                    buttonPanel.add(logInButton);
+                }
+
+                GridBagConstraints logInWindowConstraints = new GridBagConstraints();
+                logInWindowConstraints.weightx = 0;
+                logInWindowConstraints.weighty = 0;
+                logInWindowConstraints.fill = GridBagConstraints.NONE;
+                logInWindowConstraints.anchor = GridBagConstraints.CENTER;
+                logInWindowConstraints.insets = new Insets(5, 5, 0, 5);
+
+                logInWindowConstraints.gridwidth = 2;
+                mainPanel.add(welcomeLabel, logInWindowConstraints);
+
+                logInWindowConstraints.gridy = 1;
+                mainPanel.add(iconLogo, logInWindowConstraints);
+
+                logInWindowConstraints.gridwidth = 1;
+                logInWindowConstraints.anchor = GridBagConstraints.LINE_START;
+
+                logInWindowConstraints.gridy = 2;
+                mainPanel.add(new JLabel("Username: "), logInWindowConstraints);
+
+                logInWindowConstraints.gridy = 3;
+                mainPanel.add(new JLabel("Password: "), logInWindowConstraints);
+
+                logInWindowConstraints.gridx = 1;
+                logInWindowConstraints.gridy = 2;
+                logInWindowConstraints.weightx = 1;
+                logInWindowConstraints.fill = GridBagConstraints.HORIZONTAL;
+                mainPanel.add(userNameField, logInWindowConstraints);
+
+                logInWindowConstraints.gridy = 3;
+                mainPanel.add(passwordField, logInWindowConstraints);
+                
+                logInWindowConstraints.gridx = 0;
+                logInWindowConstraints.gridwidth = 2;
+                logInWindowConstraints.fill = GridBagConstraints.NONE;
+                logInWindowConstraints.anchor = GridBagConstraints.CENTER;
+
+                logInWindowConstraints.gridy = 4;
+                mainPanel.add(rememberPasswordCheckBox, logInWindowConstraints);
+
+                logInWindowConstraints.gridy = 5;
+                mainPanel.add(buttonPanel, logInWindowConstraints);
+            }
+            // Put the call to add the nested components here
+            add(mainPanel);
         }
     }
 }
