0c9beb05d22c2912ec2a612f33d462553f961219
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2022-06-05 11:18:39 +0800

    Added password show and hide icons

--- src/com/Group7/SpringStep/UI/PasswordTextField.java
+++ src/com/Group7/SpringStep/UI/PasswordTextField.java
@@ -1,24 +1,61 @@
 package com.Group7.SpringStep.ui;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import com.Group7.SpringStep.App;
+
+import net.coobird.thumbnailator.Thumbnails;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class PasswordTextField extends JPanel implements ActionListener
 {
     private boolean passwordIsVisible = false;
     private JPasswordField passwordField;
     private JButton passwordVisibilityButton;
+    private ImageIcon passwordHiddenIcon;
+    private ImageIcon passwordShownIcon;
 
     public PasswordTextField()
     {
+        
+        Image passwordHiddenSourceImage = null;
+        Image passwordShownSourceImage = null;
+        try 
+        {
+            passwordHiddenSourceImage = Thumbnails.of(App.resources.get("Closed_Eye_Icon_256.png"))
+                                                        .size(32, 32)
+                                                        .asBufferedImage();
+            passwordShownSourceImage = Thumbnails.of(App.resources.get("Opened_Eye_Icon_256.png"))
+                                                        .size(32, 32)
+                                                        .asBufferedImage();
+        } catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+
+        if (passwordHiddenSourceImage != null)
+        {
+            passwordHiddenIcon = new ImageIcon(passwordHiddenSourceImage);
+        }
+
+        if (passwordShownSourceImage != null)
+        {
+            passwordShownIcon = new ImageIcon(passwordShownSourceImage);
+        }
+
         setLayout(new BorderLayout());
         {
             passwordField = new JPasswordField();
             passwordField.setEchoChar('*');
-            passwordVisibilityButton = new JButton("Show");
+            passwordVisibilityButton = new JButton(passwordHiddenIcon);
+            passwordVisibilityButton.setContentAreaFilled(false);
+            passwordVisibilityButton.setBorderPainted(false);
+            passwordVisibilityButton.setMargin(new Insets(0, 0, 0, 0));
             passwordVisibilityButton.addActionListener(this);
             add(passwordField, BorderLayout.CENTER);
             add(passwordVisibilityButton, BorderLayout.LINE_END);
@@ -31,11 +68,11 @@ public class PasswordTextField extends JPanel implements ActionListener
         if (!passwordIsVisible) 
         {
             passwordField.setEchoChar((char) 0);
-            passwordVisibilityButton.setText("Hide");
+            passwordVisibilityButton.setIcon(passwordShownIcon);
         } else 
         {
             passwordField.setEchoChar('*');
-            passwordVisibilityButton.setText("Show");
+            passwordVisibilityButton.setIcon(passwordHiddenIcon);
         }
         passwordIsVisible = !passwordIsVisible;
     }
