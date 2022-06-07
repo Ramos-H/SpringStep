cee9b01bce751870fdf3d81abb6ca22552ba9c06
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2022-06-07 13:16:55 +0800

    Restored file save error mesage on signup window

--- src/com/Group7/SpringStep/UI/SignUpWindow.java
+++ src/com/Group7/SpringStep/UI/SignUpWindow.java
@@ -205,7 +205,7 @@ public class SignUpWindow extends JFrame implements ActionListener
             catch (Exception e1) 
             {
                 String fileSaveErrorMessage = "An error has occured: File can't be accessed or can't be found.\nPlease try again";
-                JOptionPane.showMessageDialog(null, e1.getMessage(), "Save Record Unsuccessful",
+                JOptionPane.showMessageDialog(null, fileSaveErrorMessage, "Save Record Unsuccessful",
                         JOptionPane.ERROR_MESSAGE);
                 e1.printStackTrace();
                 return;
