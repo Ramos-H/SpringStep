3491c35dc08fbb94e0b8f2a95aaa2003b52e2e4c
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2022-06-07 13:40:08 +0800

    Reorganized BoardDetails

--- src/com/Group7/SpringStep/data/BoardDetails.java
+++ src/com/Group7/SpringStep/data/BoardDetails.java
@@ -15,78 +15,17 @@ public class BoardDetails
         doneList = new ArrayList<>();
     }
 
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public ArrayList<TaskDetails> getTodoList() { return todoList; }
+    public ArrayList<TaskDetails> getDoneList() { return doneList; }
     
-    /**
-     * @return the todoList
-     */
-    public ArrayList<TaskDetails> getTodoList() 
-    {
-        return todoList;
-    }
-
-    /**
-     * @return the doneList
-     */
-    public ArrayList<TaskDetails> getDoneList() 
-    {
-        return doneList;
-    }
-
-    /**
-     * @param todoList the todoList to set
-     */
-    public void setTodoList(ArrayList<TaskDetails> todoList) 
-    {
-        this.todoList = todoList;
-    }
-
-    /**
-     * @param doneList the doneList to set
-     */
-    public void setDoneList(ArrayList<TaskDetails> doneList) 
-    {
-        this.doneList = doneList;
-    }
-
+    public void setName(String name) { this.name = name; }
+    public void setTodoList(ArrayList<TaskDetails> todoList) { this.todoList = todoList; }
+    public void setDoneList(ArrayList<TaskDetails> doneList) { this.doneList = doneList; }
     public void setLists(ArrayList<TaskDetails> newTodoList, ArrayList<TaskDetails> newDoneList)
     {
         todoList = newTodoList;
         doneList = newDoneList;
-    }
-
-    public void parseTasksFromCsv(List<String> csvs, int toDoCount, int doneCount)
-    {
-        int readOffset = 0;
-        // Read TO DO tasks
-        ArrayList<TaskDetails> newTodoList = new ArrayList<>(toDoCount);
-        for (int toDoIndex = 0; toDoIndex < toDoCount; toDoIndex++, readOffset++) 
-        {
-            TaskDetails newTask = new TaskDetails(csvs.get(readOffset));
-            newTodoList.add(newTask);
-        }
-        setTodoList(newTodoList);
-
-        // Read DONE tasks
-        ArrayList<TaskDetails> newDoneList = new ArrayList<>(doneCount);
-        for (int doneIndex = 0; doneIndex < doneCount; doneIndex++, readOffset++) 
-        {
-            TaskDetails newTask = new TaskDetails(csvs.get(readOffset));
-            newDoneList.add(newTask);
-        }
-        setDoneList(newDoneList);
     }
 
     public String formatAsCsv()
@@ -112,5 +51,27 @@ public class BoardDetails
         }
         
         return boardOutput;
+    }
+
+    public void parseTasksFromCsv(List<String> csvs, int toDoCount, int doneCount)
+    {
+        int readOffset = 0;
+        // Read TO DO tasks
+        ArrayList<TaskDetails> newTodoList = new ArrayList<>(toDoCount);
+        for (int toDoIndex = 0; toDoIndex < toDoCount; toDoIndex++, readOffset++) 
+        {
+            TaskDetails newTask = new TaskDetails(csvs.get(readOffset));
+            newTodoList.add(newTask);
+        }
+        setTodoList(newTodoList);
+
+        // Read DONE tasks
+        ArrayList<TaskDetails> newDoneList = new ArrayList<>(doneCount);
+        for (int doneIndex = 0; doneIndex < doneCount; doneIndex++, readOffset++) 
+        {
+            TaskDetails newTask = new TaskDetails(csvs.get(readOffset));
+            newDoneList.add(newTask);
+        }
+        setDoneList(newDoneList);
     }
 }
